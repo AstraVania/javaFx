d4a81e4222b333f85b6e4b1d07cbdd496d578b24
Oren Barzam <105641606+AstraVania@users.noreply.github.com>
2022-12-05 16:07:09 +0200

    Add files via upload

---
+++ hanging man game/wordStorage.java
@@ -0,0 +1,40 @@
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Random;
+
+
+public class wordStorage 
+{
+
+private String rendomWard;
+
+  public wordStorage() 
+  {
+	  randomWordFromFile();
+  }
+  
+  public String getWord()//get the random word the where chosen
+  {
+	  return rendomWard;
+  }
+  private void randomWordFromFile()
+  {
+	  long lines = 0;
+	  final Random random = new Random();
+	  try {
+
+		  Path path = Paths.get("wordbank.txt");
+	      lines = Files.lines(path).count();
+	      rendomWard = Files.readAllLines(Paths.get("wordbank.txt")).get(random.nextInt((int)lines));//count number of lines and find rend word
+	      
+	      } 
+	      catch (IOException e) 
+	      {
+	    	  System.out.println("FILE NOT FOUND");	      
+	      }
+  }
+  }
+
